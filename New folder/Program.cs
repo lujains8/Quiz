cfaf5dd2ebeaaa9cd7bdd7a7ce68eed393f98eea
lujain8 <lujains477@gmail.com>
2024-11-25 12:54:57 +0300

    quiz1

---
+++ New folder/Program.cs
@@ -0,0 +1,87 @@
+﻿/*using System;
+int num [1, 2, 3, 4, 5];
+num.Length;/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*namespace Name;
+class Program{
+public static void Main(String[] args){
+
+String Name = new string("lujain");
+int age = int 15;
+double hourly = double 34.5;
+
+
+Console.WriteLine("lujain" , 15 , 34.5);
+}
+}/*
+
+
+
+/*using System;
+namespace DayOfWeek;
+class Program{
+
+public static void Main(string[] args){
+int DayOfWeek = 4;
+
+switch(DayOfWeek)
+{
+    case 1:
+    Console.WriteLine("sunday");
+    break;
+
+    case 2:
+    Console.WriteLine("monday");
+    break;
+
+    case 3:
+    Console.WriteLine("tuseday");
+    break;
+
+    case 4:
+    Console.WriteLine("wensday");
+    break;
+
+    default:
+    Console.WriteLine("exit");
+    break;
+}
+}
+}/*
+
+
+/*class Student 
+{
+    String name = ("lujain");
+    int age = 22;
+    String grade("A");
+}
+
+public static void Main(String[] args){
+
+Student method StudentInfo($ "name: (name), age: (age), grade: (grade)");
+Console.WriteLine(Studen)
+}*/
